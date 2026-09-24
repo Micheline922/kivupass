ef2b46cdcc02dafc290cc1485d90b275bcdd5bb9
Micheline922 <michelinekalehezo@gmail.com>
2026-09-24 16:34:52 +0200

    RTE

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 declare(strict_types=1);
 
-define('DB_HOST', getenv('DB_HOST') ?: '127.0.0.1');
-define('DB_PORT', (int)(getenv('DB_PORT') ?: 3307));
-define('DB_NAME', getenv('DB_NAME') ?: 'kivupass');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: (getenv('DB_PASSWORD') ?: ''));
+define('DB_HOST', getenv('MYSQL_ADDON_HOST') ?: (getenv('DB_HOST') ?: 'boyq44fnxfe8elvqtivg-mysql.services.clever-cloud.com'));
+define('DB_PORT', (int)(getenv('MYSQL_ADDON_PORT') ?: (getenv('DB_PORT') ?: 3306)));
+define('DB_NAME', getenv('MYSQL_ADDON_DB') ?: (getenv('DB_NAME') ?: 'boyq44fnxfe8elvqtivg'));
+define('DB_USER', getenv('MYSQL_ADDON_USER') ?: (getenv('DB_USER') ?: 'uxhd9vzgcujluir2'));
+define('DB_PASS', getenv('MYSQL_ADDON_PASSWORD') ?: (getenv('DB_PASS') ?: (getenv('DB_PASSWORD') ?: 'STBVNBSvvCXg5pgd6ags')));
 const SUPER_ADMIN_PASSWORD_HASH = '$2y$10$BB3YCCj7kXsC0PhEzfJ/JOQ94Yiz07Qaw0uS23hSCA06.sBth58ey';
 
 function startAppSession(): void
